82f826adb5b1d04ca9809c7a14bfe7d4689bfaba
heyolaniran <olaniran.abd@gmail.com>
2025-09-08 09:40:52 +0100

    feat: adding animations to hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Hero from "./components/ui/hero"
 import NavBar from "./components/ui/nav-bar"
 
 function App() {
   return (
     <main>
       <NavBar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
